09c1f882060b49d444272fec51132a84c177dc0e
Ahmad Salihu <Ahmadsalihu419@gmail.com>
2021-02-07 00:06:49 +0100

    mangal on the progress

---
+++ backend/data.js
@@ -0,0 +1,102 @@
+const data = {
+  products : [
+    {
+      _id: 1,
+      name: 'Nike Slim Shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 100,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: '4.5',
+      numReviews: 11,
+      description: 'High Quality Slim Shirts'
+    },
+    {
+      _id: 2,
+      name: 'Adidas Slim Shirts',
+      category: 'Slim fit',
+      image: '/images/p2.jpg',
+      price: 90,countInStock: 12,
+      brand: 'Adidas',
+      rating: '4.5',
+      numReviews: 5,
+      description: 'High Quality Slim fit'
+    },
+    {
+      _id: 3,
+      name: 'Gucci',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 65,
+      brand: 'Gucci',
+      countInStock: 0,
+      rating: '4.5',
+      numReviews: 20,
+      description: 'High Quality Slim Shirts'
+    },
+    {
+      _id: 4,
+      name: 'Versace Shirts',
+      category: ' VersaceShirts',
+      image: '/images/p4.jpg',
+      price: 100,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: '4.5',
+      numReviews: 100,
+      description: 'High Quality Slim Shirts'
+    },
+    {
+      _id: 5,
+      name: 'Puma Slim Shirts',
+      category: 'Puma Shirts',
+      image: '/images/p5.jpg',
+      price: 120,
+      countInStock: 20,
+      brand: 'Puma',
+      rating: '4.5',
+      numReviews: 8,
+      description: 'High Quality Slim Shirts'
+    },
+    {
+      _id: 6,
+      name: 'Nike Slim pants',
+      category: 'Pants',
+      image: '/images/p6.jpg',
+      price: 50,
+      countInStock: 9,
+      brand: 'Nike',
+      rating: '4.5',
+      numReviews: 15,
+      description: 'High Quality Slim Shirts'
+    },
+   
+    {
+      _id: 7,
+      name: 'Zara short Sleeve',
+      category: 'Shirts',
+      image: '/images/p7.jpg',
+      price: 150,
+      countInStock: 25,
+      brand: 'Zara',
+      rating: '4.5',
+      numReviews: 10,
+      description: 'High Quality Zara Short Sleeve'
+    },
+    {
+      _id: 8,
+      name: 'Zara Slim Shirts',
+      category: 'Shirts',
+      image: '/images/p8.jpg',
+      price: 35,
+      countInStock: 13,
+      brand: 'Zara',
+      rating: '4.5',
+      numReviews: 10,
+      description: 'High Quality Zara Short Sleeve Shirts'
+    },
+  ],
+};
+
+export default data;
